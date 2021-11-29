fa1d0b5f049f03b27956cae4ddabdaf5fa77d959
luissancar <luissancar@yahoo.es>
2021-11-29 13:49:16 +0100

    Dos

--- app/src/main/java/com/example/notificacionesjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/notificacionesjetpackcompose/MainActivity.kt
@@ -3,25 +3,24 @@ package com.example.notificacionesjetpackcompose
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.notificacionesjetpackcompose.ui.theme.NotificacionesJetpackComposeTheme
@@ -47,25 +46,136 @@ fun Greeting() {
     val context = LocalContext.current
     val channelId = "Channel"
     val notificationId = 0
+
+    val myBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.nosfe)
+
    // val myBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_foreground)
     //val bigText = "Contrary to popular belief, lorem ipsum is not simply random text. It has roots in a piece of classical latin literature from 45 BC, making it over 2000 year old."
 
     LaunchedEffect(Unit) {
         createNotificationChannel(channelId, context)
     }
-    Column (modifier = Modifier.fillMaxSize()) {
+    Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally ) {
        Button(
            modifier = Modifier
                .padding(20.dp)
-               .fillMaxWidth(),
+               .size(100.dp),
+
+           colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow),
+           shape = RoundedCornerShape(25),
            onClick = {
                simpleNotification(channelId = channelId, context = context, notificationId = notificationId, textContent = "Notificación Simple", textTitle = "Notificación")
 
            }) {
-           Text(text = "notificación")
+           Text(text = "Notificación Simple")
            
        }
+
+
+
+        Button(
+            modifier = Modifier
+                .padding(20.dp)
+                .size(100.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+            shape = RoundedCornerShape(50),
+            onClick = {
+                textoLargoNotification(channelId = channelId, context = context, notificationId = notificationId,
+                    textContent = "Notificación texto largo, Notificación texto largo, Notificación texto largo, Notificación texto largo, Notificación texto largo, Notificación texto largo, ",
+                    textTitle = "Notificación")
+
+            }) {
+            Text(text = "Notificación texto largo")
+
+        }
+
+
+
+        Button(
+            modifier = Modifier
+                .padding(20.dp)
+                .size(100.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green),
+            shape = RoundedCornerShape(10),
+            onClick = {
+                textoConIconoNotificacion(iconoGrande = myBitmap, channelId = channelId, context = context, notificationId = notificationId, textContent = "Notificación con icono", textTitle = "Notificación",)
+
+            }) {
+            Text(text = "Notificación con icono")
+
+        }
+
+
+        Button(
+            modifier = Modifier
+                .padding(20.dp)
+                .size(100.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray),
+            shape = RoundedCornerShape(5),
+            onClick = {
+                imagenGrandeNotification(iconoGrande = myBitmap, channelId = channelId, context = context, notificationId = notificationId, textContent = "Notificación imagen grande", textTitle = "Notificación",)
+
+            }) {
+            Text(text = "Notificación imagen grande")
+
+        }
+
+
    }
+}
+
+
+
+fun imagenGrandeNotification(
+    context: Context,
+    channelId: String,
+    notificationId: Int,
+    textTitle: String,
+    textContent: String,
+    iconoGrande: Bitmap,
+    priority: Int = NotificationCompat.PRIORITY_DEFAULT
+) {
+    val builder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.notificacion)
+        .setContentTitle(textTitle)
+        .setContentText(textContent)
+        .setLargeIcon(iconoGrande)
+        .setStyle(
+            NotificationCompat.BigPictureStyle()
+                .bigPicture(iconoGrande)
+                .bigLargeIcon(null)
+        )
+        .setPriority(priority)
+    with(NotificationManagerCompat.from(context)) {
+        notify(notificationId, builder.build())
+    }
+}
+
+
+fun textoConIconoNotificacion(
+    context: Context,
+    channelId: String,
+    notificationId: Int,
+    textTitle: String,
+    textContent: String,
+    iconoGrande: Bitmap,
+    priority: Int = NotificationCompat.PRIORITY_DEFAULT
+) {
+    val builder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.notificacion)
+        .setContentTitle(textTitle)
+        .setContentText(textContent)
+        .setLargeIcon(iconoGrande)
+        .setStyle(
+            NotificationCompat.BigTextStyle()
+                .bigText(
+                    textContent
+                )
+        )
+        .setPriority(priority)
+    with(NotificationManagerCompat.from(context)) {
+        notify(notificationId, builder.build())
+    }
 }
 
 
@@ -82,6 +192,29 @@ fun simpleNotification(
         .setSmallIcon(R.drawable.notificacion)
         .setContentTitle(textTitle)
         .setContentText(textContent)
+        .setPriority(priority)
+    with(NotificationManagerCompat.from(context)) {
+        notify(notificationId, builder.build())
+    }
+}
+
+
+fun textoLargoNotification(
+    context: Context,
+    channelId: String,
+    notificationId: Int,
+    textTitle: String,
+    textContent: String,
+    priority: Int = NotificationCompat.PRIORITY_DEFAULT
+) {
+    val builder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.notificacion)
+        .setContentTitle(textTitle)
+        .setContentText(textContent)
+        .setStyle(
+            NotificationCompat.BigTextStyle()
+                .bigText(textContent)
+        )
         .setPriority(priority)
     with(NotificationManagerCompat.from(context)) {
         notify(notificationId, builder.build())
